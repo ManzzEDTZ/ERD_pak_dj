cf94cffbc4aebb3d33c1eb1e0be50ce668d8f6f4
Celxd <celxd250606@gmail.com>
2023-02-01 13:43:23 +0700

    Navbar no login

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -14,19 +14,19 @@
     <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <div class="row">
+            <div class="row align-items-center">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <div class="col">
+                    <div class="col-auto">
                         <a class="navbar-brand mr-5" href="#"><embed src="{{ asset('assets/image/logo.svg') }}"></a>
                     </div>
-                    <div class="col-10">
+                    <div class="col-8">
                         <ul class="navbar-nav">
-                            <li class="nav-item" style="min-width: 18rem;">
+                            <li class="nav-item" style="min-width: 50rem;">
                                 <div class="input-group">
-                                    <input class="form-control py-2 border-right-0 border" type="search" placeholder="Search for anything" id="example-search-input">
+                                    <input class="form-control py-2 border-right-0 border" type="search" placeholder="Search for anything" id="search-input">
                                     <span class="input-group-append">
                                         <button class="btn btn-outline-secondary border-left-0 border" type="button">
                                             <i class="fa fa-search"></i>
@@ -36,11 +36,20 @@
                             </li>
                         </ul>
                     </div>
-                    <div class="col">
+                    <div class="col-2">
+                        <a href="" style="color:black">Mengajar Di Ruscation</a>
+                        
+                    </div>
+                    <div class="col-auto">
+                        <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </button>
+                    </div>
+                    <div class="col-auto me-auto">
                         <button type="button" class="btn btn-outline-dark">Sign Up</button>
                         
                     </div>
-                    <div class="col">
+                    <div class="col-auto">
                         <button type="button" class="btn btn-dark">Log In</button>
                     </div>
                 </div>
