027f79c815f77c889b824de2277416fc936cb08c
Celxd <celxd250606@gmail.com>
2023-02-15 14:23:30 +0700

    Fix sizing

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -1,72 +1,59 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar">
     <div class="container-fluid">
-        <div class="row align-items-center">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="col-auto">
-                    <a class="navbar-brand mr-5" href="#"><embed src="{{ asset('assets/image/logo.svg') }}"></a>
+        <div class="row align-items-center container-fluid">
+            <div class="col-auto">
+                <a class="navbar-brand mr-5" href="#"><embed src="{{ asset('assets/image/logo.svg') }}"></a>
+            </div>
+            <div class="col">
+                <div class="input-group" >
+                    <input type="text" class="form-control" placeholder="Search ......" aria-label="" style="width: 80%;">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="fa fa-search"></i></span>
+                    </div>
                 </div>
-                <div class="col-8">
-                    <ul class="navbar-nav">
-                        <li class="nav-item" style="min-width: 50rem;">
-                            <div class="input-group">
-                                <input class="form-control py-2 border-right-0 border" type="search" placeholder="Search for anything" id="search-input">
-                                <span class="input-group-append">
-                                    <button class="btn btn-outline-secondary border-left-0 border" type="button">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2">
-                    <a href="" style="color:black">Mengajar Di Ruscation</a>
-                    
-                </div>
-                @guest
-                <div class="col-auto">
-                    <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </button>
-                </div>
-                <div class="col-auto me-auto">
+            </div>
+            <div class="col-auto">
+                <a href="" style="color:black">Mengajar Di Ruscation</a>
+            </div>
+            @guest
+            <div class="col-auto">
+                <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </button>
+            </div>
+            <div class="col-auto">
                     <button type="button" class="btn btn-outline-dark">Sign Up</button>
                     
-                </div>
-                <div class="col-auto">
-                    <button type="button" class="btn btn-dark">Log In</button>
-                </div>
-                @endguest
-                @auth
-                <div class="col-auto">
-                        <a href="" style="color:black">Pelajaran Saya</a>
-                        
-                </div>
-                <div class="col-auto">
-                    <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
-                        <i class="fa-regular fa-heart"></i>
-                    </button>
-                </div>
-                <div class="col-auto">
-                    <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </button>
-                </div>
-                <div class="col-auto">
-                    <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
-                        <i class="fa-regular fa-bell"></i>
-                    </button>
-                </div>
-                <div class="col-auto">
-                    <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
-                        <i class="fa-regular fa-user"></i>
-                    </button>
-                </div>
-                @endauth
             </div>
+            <div class="col-auto">
+                <button type="button" class="btn btn-dark">Log In</button>
+            </div>
+            @endguest
+            @auth
+            <div class="col-auto">
+                        <a href="" style="color:black">Pelajaran Saya</a>
+            </div>
+            <div class="col-auto">
+                <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
+                    <i class="fa-regular fa-heart"></i>
+                </button>
+            </div>
+            <div class="col-auto">
+                <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </button>
+            </div>
+            <div class="col-auto">
+                <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
+                    <i class="fa-regular fa-bell"></i>
+                </button>
+            </div>
+            <div class="col-auto">
+                <button class="btn border-left-0 border-0" type="button" style="background-color:transparent;">
+                    <i class="fa-regular fa-user"></i>
+                </button>
+            </div>
+            @endauth
         </div>
     </div>
 </nav>
